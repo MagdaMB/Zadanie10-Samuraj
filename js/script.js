1f66cbe2e5cdef9e85a4bbdcee63cfcc467c089d
MagdaMB <magda.m.badura@gmail.com>
2019-01-07 21:35:01 +0100

    timer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-const add = (start = 0) => {
-    let number = start;
-    return () => {
-        number++;
-        document.body.textContent = number;
-    }
+const countTime = (num = 0) => {
+    let numOfSecunds = num;
+    document.body.textContent = `minęło ${numOfSecunds}s odkąd jesteś na stronie`;
+
+    function clock() {
+        numOfSecunds++;
+        document.body.textContent = `minęło ${numOfSecunds}s odkąd jesteś na stronie`;
+    };
+
+    return clock;
 }
 
-const counter = add();
-document.addEventListener('click', counter)
+const timer = countTime();
+
+setInterval(timer, 1000)
